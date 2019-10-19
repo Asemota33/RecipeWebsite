73dbcd1f5e0c3925ce325b73bc39c6ac09c48f7e
Michael Asemota <52172210+Asemota33@users.noreply.github.com>
2019-10-19 10:55:04 -0400

    Add files via upload
    Added functionality that sends information from list to display page

---
+++ Comp229-301052117-Assign02/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Comp229_301052117_Assign02.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Comp229_301052117_Assign02.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult DisplayPage(string dishName)
+        {
+            foreach (var r in RecipeRepository.recipe)
+            {
+                if (dishName == r.DishName)
+                {
+                    return View(r);
+                }
+            }
+
+            return View(null);
+        }
+
+        public ViewResult UserPage()
+        {
+            return View("UserPage");
+        }
+        public ViewResult DataPage()
+        {
+            return View("DataPage", RecipeRepository.recipes);
+        }
+        [HttpGet]
+        public ViewResult InsertPage()
+        {
+            return View("InsertPage");
+        }
+
+        [HttpPost]
+        public ViewResult InsertPage(Recipes recipes)
+        {
+            RecipeRepository.AddRecipe(recipes);
+            return View("DataPage", RecipeRepository.recipes);
+        }
+    }
+    
+}
---
+++ Comp229-301052117-Assign02/Models/Recipes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Comp229_301052117_Assign02.Models
+{
+    public class Recipes
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Author { get; set; }
+        [Required(ErrorMessage = "Please choose the amount of servings")]
+        public string Servings { get; set; }
+        [Required(ErrorMessage = "Please enter your dish name")]
+        public string DishName { get; set; }
+        [Required(ErrorMessage = "Please enter the ingredients")]
+        public string Ingredients { get; set; }
+        [Required(ErrorMessage = "Please enter the description of your dish")]
+        public string Description { get; set; }
+        [Required(ErrorMessage = "Please choose a category")]
+        public string Category { get; set; }
+        //public int ID { get; set; }
+
+    }
+}
